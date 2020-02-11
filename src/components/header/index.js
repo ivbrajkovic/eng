a632d213dad548d1d4213d21d52c48dfcadc54fd
Ivan Brajković <44271953+ivbrajkovic@users.noreply.github.com>
2020-02-11 19:22:30 +0100

    add telefon

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,15 @@
 import { h } from 'preact';
 
 const Header = () => {
-    return (
-        <nav class="navbar navbar-dark bg-dark">
-            <span class="navbar-brand mb-0">
-                <h3>
-                    <em>FastENG</em>
-                </h3>
-            </span>
-        </nav>
-    );
+  return (
+    <nav class="navbar navbar-dark bg-dark shadow">
+      <span class="navbar-brand mb-0">
+        <h3>
+          <em>FastENG</em>
+        </h3>
+      </span>
+    </nav>
+  );
 };
 
 export default Header;
